dc608dc03cfc8214cc8304253f91b1851a9a5bcf
Lars Gerrit Kliesing <lgk@patorg.org>
2018-01-29 12:42:57 +0100

    first tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { LgkPillComponent } from "lgk";
 
+const Sass = require("./sass");
+Sass.setWorkerUrl("sass.worker.js");
+
+const sass = new Sass();
+
+const style = `
+$bgcolor: #ccc;
+
+body { background-color: $bgcolor; }
+`;
+
+sass.compile(style, result => {
+    console.log(result);
+
+    document.getElementById("style").innerHTML = result.text;
+});
+
 class App extends React.Component {
     render() {
         return (
