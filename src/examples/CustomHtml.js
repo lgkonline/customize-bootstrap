1bacfbaac590b965a24980475650bedf6398f5a6
Lars Kliesing <larskliesing@larss-mbp.speedport.ip>
2019-10-12 22:04:01 +0200

    html editor

--- src/examples/CustomHtml.js
+++ src/examples/CustomHtml.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
+import { CodeFlaskReact } from "../components/CodeFlaskReact";
+
 export function CustomHtml() {
     const [html, setHtml] = useState(`<h1 class="mt-4 mb-3">Replace this...</h1>\n<button class="btn btn-primary btn-lg">...with your own code!</button>`);
 
     return (
         <div>
-            <textarea className="form-control" value={html} onChange={({ target }) => setHtml(target.value)} />
+            <div className="position-relative mb-3" style={{ minHeight: "200px" }}>
+                <CodeFlaskReact
+                    code={html}
+                    onChange={setHtml}
+                />
+            </div>
 
             <div dangerouslySetInnerHTML={{ __html: html }} />
         </div>
